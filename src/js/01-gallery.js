18043e7293e855d4ac282d3fe5d3ffc9c3d50325
Evgeniy <evgnec@gmail.com>
2023-04-01 21:51:24 +0300

    2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,8 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
 // Change code below this line
+
 const gallery = document.querySelector('.gallery');
 
 const galleryIt = galleryItems.map(function ({ preview, original, description }) {
@@ -9,7 +11,6 @@ const galleryIt = galleryItems.map(function ({ preview, original, description })
         <img 
     class = "gallery__image"
     src="${preview}"
-    data-source="${original}" 
     alt="${description}"
          />
     </a>
@@ -19,29 +20,8 @@ const galleryIt = galleryItems.map(function ({ preview, original, description })
 
 gallery.insertAdjacentHTML('beforeend', galleryIt);
 
-gallery.addEventListener('click', clickImage);
-
-
-function clickImage(event) {
-
-    event.preventDefault();
- 
-    const imgInstance = event.target;
-    if (imgInstance.nodeName !== 'IMG') {
-        return;
-    }
-    const instance = basicLightbox.create(`
-    <img class="gallery__image" src="${imgInstance.dataset.source}" width="800" height="600">
-`)
-    instance.show();
- window.addEventListener('keydown', onCloseModalClick);
-}
-
-function onCloseModalClick(event) {
-    const openInstance = document.querySelector(".basicLightbox")
-    if (event.code === "Escape") {
-        openInstance.remove();
-           window.removeEventListener('keydown', onCloseModalClick);  
-    }
-
-}
+new SimpleLightbox('.gallery__link',
+        {   captionsData: 'alt',
+            captionPosition: 'bottom',
+            captionDelay: 250,
+        });
