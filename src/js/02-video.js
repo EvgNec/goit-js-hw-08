948cd66176994de0dfba4df69c8394c78262c3a7
Evgeniy <evgnec@gmail.com>
2023-04-04 23:19:11 +0300

    add

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+// import throttle from 'lodash.throttle';
 
+
+//   <script src="https://player.vimeo.com/api/player.js"></script> 
+
+const iframe = document.getElementById('vimeo-player');
+    const player = new Player(iframe);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+
+   const iframe2 = document.getElementById('vimeo-player2');
+    const player2 = new Player(iframe2);
+
+    player2.on('play', function() {
+        console.log('played the video!');
+    });
+
+    player2.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
